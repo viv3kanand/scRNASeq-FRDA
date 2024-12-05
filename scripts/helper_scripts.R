12192cb4779d87d1d7d12729581bbac844fc1371
viv3kanand <viv3kanand@gmail.com>
2024-12-05 14:20:24 +0000

    dcats function for proportion analysis and renv snapshot

--- scripts/helper_scripts.R
+++ scripts/helper_scripts.R
@@ -37,3 +37,25 @@ knee_plot <- function(bc_rank) {
     labs(y = "Rank", x = "Total UMIs")
   return(p)
 }
+
+run_dcats <- function(obj, Condition = "Condition") {
+  require(DCATS)
+  require(tidyverse)
+  knn_mat = knn_simMat(obj@graphs$SCT_snn, obj$cell_type)
+  
+  count_mat = table(obj$orig.ident, obj$cell_type)
+  
+  design_mat = obj@meta.data |> 
+    group_by(orig.ident, Condition) |> 
+    summarize() |> 
+    tibble::column_to_rownames("orig.ident")
+  
+  res <- dcats_GLM(count_mat, design_mat, similarity_mat = knn_mat)
+  df <- as.data.frame(res)
+  colnames(df) <- names(res)
+  
+  return(df)
+}
+
+
+
