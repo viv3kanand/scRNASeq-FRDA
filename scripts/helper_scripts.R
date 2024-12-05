1d1d0a111cde6fbf18d0ee8c82cf584ead234278
viv3kanand <viv3kanand@gmail.com>
2024-12-05 14:28:15 +0000

    add function for zscore calculation

--- scripts/helper_scripts.R
+++ scripts/helper_scripts.R
@@ -38,6 +38,10 @@ knee_plot <- function(bc_rank) {
   return(p)
 }
 
+scale_this <- function(x){
+  (x - mean(x, na.rm=TRUE)) / sd(x, na.rm=TRUE)
+}
+
 run_dcats <- function(obj, Condition = "Condition") {
   require(DCATS)
   require(tidyverse)
