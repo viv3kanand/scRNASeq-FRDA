d5c76f5b3ab02aad3d475c16853f7d483ccfba68
viv3kanand <viv3kanand@gmail.com>
2025-03-17 12:06:00 +0000

    split helper scripts to dcats

--- scripts/helper_scripts.R
+++ scripts/helper_scripts.R
@@ -42,24 +42,5 @@ scale_this <- function(x){
   (x - mean(x, na.rm=TRUE)) / sd(x, na.rm=TRUE)
 }
 
-run_dcats <- function(obj, Condition = "Condition") {
-  require(DCATS)
-  require(tidyverse)
-  knn_mat = knn_simMat(obj@graphs$SCT_snn, obj$cell_type)
-  
-  count_mat = table(obj$orig.ident, obj$cell_type)
-  
-  design_mat = obj@meta.data |> 
-    group_by(orig.ident, Condition) |> 
-    summarize() |> 
-    tibble::column_to_rownames("orig.ident")
-  
-  res <- dcats_GLM(count_mat, design_mat, similarity_mat = knn_mat)
-  df <- as.data.frame(res)
-  colnames(df) <- names(res)
-  
-  return(df)
-}
-
 
 
