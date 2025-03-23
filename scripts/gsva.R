65d47747628bb5b7c61186a253cc5330056e76da
viv3kanand <viv3kanand@gmail.com>
2025-03-23 12:17:30 +0000

    apply gsva for expr list

--- scripts/gsva.R
+++ scripts/gsva.R
@@ -1,17 +1,19 @@
-require(GSVA)
-require(BiocParallel)
-require(dplyr)
-
-
-applyGSVA = function(geneset, group_col, gene_col, exprmat, kcdf = c("Gaussian", "Poisson")) {
+applyGSVA = function(geneset, group_col, gene_col, exprmatList, kcdf = c("Gaussian", "Poisson")) {
+  require(GSVA)
+  require(BiocParallel)
+  require(dplyr)
   
   stopifnot(inherits(geneset, "tbl_df") &
               inherits(group_col, "character") &
               inherits(gene_col, "character") &
-              inherits(exprmat, "matrix"))
+              inherits(exprmaListt, "matrix"))
   
   kcdf = match.arg(kcdf)
   
-  gsvapar = gsvaParam(exprData = exprmat, geneSets = geneset, kcdf = kcdf)
-  res = gsva(gsvapar, verbose = TRUE, BPPARAM = MulticoreParam(workers = 40))
+  for (i in seq_along(exprmatList)) {
+    exprmat = exprmatList[[i]]
+    
+    gsvapar = gsvaParam(exprData = exprmat, geneSets = geneset, kcdf = kcdf)
+    res = gsva(gsvapar, verbose = TRUE, BPPARAM = MulticoreParam(workers = 40))
+  }
 }
