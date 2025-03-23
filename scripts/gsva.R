6b4a67752f75b6d91ca3f755a79daf6c58a629db
viv3kanand <viv3kanand@gmail.com>
2025-03-23 12:12:12 +0000

    check param ext and select kernel

--- scripts/gsva.R
+++ scripts/gsva.R
@@ -5,7 +5,13 @@ require(dplyr)
 
 applyGSVA = function(geneset, group_col, gene_col, exprmat, kcdf = c("Gaussian", "Poisson")) {
   
+  stopifnot(inherits(geneset, "tbl_df") &
+              inherits(group_col, "character") &
+              inherits(gene_col, "character") &
+              inherits(exprmat, "matrix"))
   
-  gsvapar = gsvaParam(exprData = exprmat, geneSets = geneset, kcdf = "Gaussian")
+  kcdf = match.arg(kcdf)
+  
+  gsvapar = gsvaParam(exprData = exprmat, geneSets = geneset, kcdf = kcdf)
   res = gsva(gsvapar, verbose = TRUE, BPPARAM = MulticoreParam(workers = 40))
 }
