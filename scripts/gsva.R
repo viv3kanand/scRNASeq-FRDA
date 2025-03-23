aa83b2f1848baac3327edb9a2ce74972f100b4d1
viv3kanand <viv3kanand@gmail.com>
2025-03-23 12:09:50 +0000

    gsva param and gsva res

--- scripts/gsva.R
+++ scripts/gsva.R
@@ -5,4 +5,7 @@ require(dplyr)
 
 applyGSVA = function(geneset, group_col, gene_col, exprmat, kcdf = c("Gaussian", "Poisson")) {
   
+  
+  gsvapar = gsvaParam(exprData = exprmat, geneSets = geneset, kcdf = "Gaussian")
+  res = gsva(gsvapar, verbose = TRUE, BPPARAM = MulticoreParam(workers = 40))
 }
