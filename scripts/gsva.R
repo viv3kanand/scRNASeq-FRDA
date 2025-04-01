e6ab22e947da940165508d4a9ca6699b623690e8
viv3kanand <viv3kanand@gmail.com>
2025-04-01 10:47:18 +0000

    Fix gsva function calling list names as vectors

--- scripts/gsva.R
+++ scripts/gsva.R
@@ -1,12 +1,12 @@
-applyGSVA = function(gset_df, group_col, gene_col, expr_mat_list, kcdf = c("Gaussian", "Poisson")) {
+applyGSVA = function(gset_df, group_col, gene_col, expr_mat, kcdf = c("Gaussian", "Poisson")) {
   require(GSVA)
   require(BiocParallel)
   require(dplyr)
-  
+
   stopifnot(inherits(gset_df, "tbl_df") &
               inherits(group_col, "character") &
               inherits(gene_col, "character") &
-              inherits(expr_mat_list, "list"))
+              inherits(expr_mat_list, "tbl_df"))
   
   if (!requireNamespace("GSVA", quietly = TRUE)) {
     stop("GSVA package is required but not installed!")
@@ -14,13 +14,13 @@ applyGSVA = function(gset_df, group_col, gene_col, expr_mat_list, kcdf = c("Gaus
   
   kcdf = match.arg(kcdf)
   
-  gset_list <- split(as.character(gset_df[gene_col]), as.character(gset_df[group_col]))
+  gset_list <- split(as.character(gset_df[[gene_col]]), as.character(gset_df[[group_col]]))
   
   expr_mat = vst_gsva |> 
     tibble::column_to_rownames("symbol") |>
     as.matrix()
-  
-  gsvapar = gsvaParam(exprData = expr_mat, geneSets = gset_list, kcdf = "Gaussian")
+
+  gsvapar = gsvaParam(exprData = expr_mat, geneSets = gset_list, kcdf = kcdf)
   res = gsva(gsvapar, verbose = TRUE, BPPARAM = MulticoreParam(workers = 40))
   
   return(res)
